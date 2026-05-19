637db67f54eb8da874bc65281990cc753ea2957c
Maryamfarooq2004 <maryamfarooqkhan2004@gmail.com>
2026-05-20 03:18:42 +0500

    Polish branding and UI

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ db = client.camp2016    #Select the database
 todos = db.todo #Select the collection
 
 app = Flask(__name__)
-title = "TODO with Flask"
-heading = "ToDo Reminder"
+title = "Velvet Orbit Tasks"
+heading = "Opaline Planner"
 #modify=ObjectId()
 
 def redirect_url():
